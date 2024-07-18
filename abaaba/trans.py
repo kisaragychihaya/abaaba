dc1c5c0fa38a46ff2c81e7e2939c55684dff2e23
kisaragychihaya <hetongtech@hotmail.com>
2024-07-18 14:18:43 +0800

    增加生成html功能

--- abaaba/trans.py
+++ abaaba/trans.py
@@ -1,5 +1,5 @@
 import json,pypandoc
-
+from pathlib import Path
 class Translate:
     def __init__(self,translate=None,fl="zh",tl="en"):
         self.bot=Translate
@@ -14,8 +14,20 @@ class Translate:
                 raise NotImplementedError("找不到翻译器！！")
 
     def translate(self,src):
-        self.doc = json.loads(pypandoc.convert_file(src, "json", format="markdown",
+        self.suffix=Path(src).suffix
+        if self.suffix==".md":
+            self.doc = json.loads(pypandoc.convert_file(src, "json", format="markdown",
                                                     extra_args=["--from=markdown-markdown_in_html_blocks+raw_html"]))
+        elif self.suffix==".rst":
+            self.doc=""
+            with open(src,"r",encoding="utf-8") as f:
+                lines = f.readlines()
+                for line in lines:
+                    if len(line)and (not line[0].isascii()):
+                        self.doc+=str(self.bot.main(line, self._sl, self._tl)).strip("\n")+"\n"
+                    else:
+                        self.doc+=line
+                return
         for blk in self.doc["blocks"]:
             if blk["t"]=="RawBlock":
                 continue
@@ -34,7 +46,16 @@ class Translate:
             for content in blk:
                 self._p(content)
     def save(self,dst):
-        data=json.dumps(self.doc)
-        data=pypandoc.convert_text(data,format="json",to="markdown_strict",extra_args=["--wrap=none"])
-        with open(dst,mode="w",encoding="utf-8") as file:
+        data=self.doc
+        if self.suffix==".md":
+            data = json.dumps(self.doc)
+            data=pypandoc.convert_text(data,format="json",to="markdown_strict",extra_args=["--wrap=none"])
+        elif self.suffix==".rst":
+            pass
+        else:
+            raise NameError("Unsupported format {}".format(self.suffix))
+        with open(dst, mode="w", encoding="utf-8") as file:
             file.write(data)
+
+
+
